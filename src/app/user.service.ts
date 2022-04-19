1803debfc928ab0fb87a1018ff98c7c13b92d6d9
96olbap <alvinwanjala@gmail.com>
2022-04-19 16:09:28 +0300

    generate user service

---
+++ src/app/user.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { User } from './user';
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+
+  
+  constructor() { }
+}
