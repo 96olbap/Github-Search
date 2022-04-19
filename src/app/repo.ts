9c813cc3634d5647c56e9200c5d4bb36c9a307bb
96olbap <alvinwanjala@gmail.com>
2022-04-19 15:00:03 +0300

    generated a class repo

---
+++ src/app/repo.ts
@@ -0,0 +1,11 @@
+export class Repo {
+    constructor(
+        public name: any,
+        public full_name:any,
+        public description:any,
+        public created_at: any,
+        public html_url: any
+    ){
+        
+    }
+}
