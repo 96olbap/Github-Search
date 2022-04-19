ac4436635ff9637b4538f17813821e042551ca43
96olbap <alvinwanjala@gmail.com>
2022-04-19 15:33:37 +0300

    update search-form component

--- src/app/components/repositories/repositories.component.ts
+++ src/app/components/repositories/repositories.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Repo } from 'src/app/repo';
 
 @Component({
   selector: 'app-repositories',
@@ -6,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./repositories.component.css']
 })
 export class RepositoriesComponent implements OnInit {
+
+  repos!:Repo[];
 
   constructor() { }
 
