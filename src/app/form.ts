a4c3d17306a93a07a1bbb068a755fc11ea71c9fe
96olbap <alvinwanjala@gmail.com>
2022-04-20 02:48:07 +0300

    This commit will update the form .ts file

--- src/app/form.ts
+++ src/app/form.ts
@@ -1,2 +1,18 @@
 export class Form {
+
+    public image: any
+    public username: any
+    public bio: any
+    public followers: any
+    public following: any
+    public location: any
+    public twitter!: string
+}
+
+export class foundRepos {
+    public name: any
+    public full_name: any
+    public description: any
+    public created_at: any
+    public html_url: any
 }
