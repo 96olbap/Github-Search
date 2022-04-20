0e6760955dab308324fc76f559030f9b08b05594
96olbap <alvinwanjala@gmail.com>
2022-04-20 04:51:40 +0300

    This commit will update the routing module and define two new paths

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SearchFormComponent } from './components/search-form/search-form.component';
+import { RepositoriesComponent } from './components/repositories/repositories.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'search-form', component: SearchFormComponent},
+  {path: 'repositories', component: RepositoriesComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
